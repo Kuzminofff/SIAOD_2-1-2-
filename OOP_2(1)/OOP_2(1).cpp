8d84b60bf03cbd21c759039b3eb47d47dcbace38
Denis Kuzminov <dekuzminov@sfedu.ru>
2024-12-27 14:21:01 +0300

    Add project files.

---
+++ OOP_2(1)/OOP_2(1).cpp
@@ -0,0 +1,66 @@
+﻿// OOP_2(1).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+﻿#include <iostream>
+#include <set>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int array[100];
+    srand(time(0));
+
+    for (int i = 0; i < 100; i++) {
+        array[i] = rand() % 99 + 1;
+    }
+
+    cout << "Элементы массива: " << endl;
+    for (int i = 0; i < 100; i++) {
+        cout << array[i] << endl;
+    }
+
+    set<int> BalancedTree;
+
+    for (int i = 0; i < 100; i++) {
+        BalancedTree.insert(array[i]);
+    }
+
+    cout << "Элементы дерева: ";
+    for (auto& element : BalancedTree) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    int value;
+    cout << "Введите значение для поиска: ";
+    cin >> value;
+
+    auto it = BalancedTree.find(value);
+
+    if (it != BalancedTree.end()) {
+        int index = 0;
+        for (auto element : BalancedTree) {
+            if (element == value) {
+                cout << "Число " << value << " найдено в индексе: " << index << endl;
+                break;
+            }
+            index++;
+        }
+    }
+    else {
+        cout << "Число " << value << " не найдено." << endl;
+    }
+
+    return 0;
+}
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ OOP_2(1)/OOP_2(2).cpp
@@ -0,0 +1,98 @@
+﻿﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+struct TrieNode {
+    unordered_map<char, TrieNode*> children;
+    bool isEndOfWord;
+
+    TrieNode() : isEndOfWord(false) {}
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() { root = new TrieNode(); }
+
+    void insert(const string& word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                node->children[c] = new TrieNode();
+            }
+            node = node->children[c];
+        }
+        node->isEndOfWord = true;
+    }
+
+    bool search(const string& word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                return false;
+            }
+            node = node->children[c];
+        }
+        return node->isEndOfWord;
+    }
+
+    void printTrie(TrieNode* node, string prefix) {
+        if (node->isEndOfWord) {
+            cout << prefix << endl;
+        }
+        for (auto& pair : node->children) {
+            printTrie(pair.second, prefix + pair.first);
+        }
+    }
+};
+
+
+string generateRandomWord() {
+    string word;
+    for (int i = 0; i < 10; i++) {
+        char c = 'A' + rand() % 4;
+        word += c;
+    }
+    return word;
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    srand(time(0));
+
+    vector<string> words;
+    Trie trie;
+
+    for (int i = 0; i < 30; i++) {
+        string word = generateRandomWord();
+        words.push_back(word);
+        trie.insert(word);
+    }
+
+    cout << "Сгенерированные слова:\n";
+    for (const auto& w : words) {
+        cout << w << endl;
+    }
+
+    cout << "\nПрефиксное дерево:\n";
+    trie.printTrie(trie.root, "");
+
+    string inputWord;
+    cout << "\nВведите слово для поиска: ";
+    cin >> inputWord;
+
+    if (trie.search(inputWord)) {
+        cout << "Слово \"" << inputWord << "\" найдено в дереве." << endl;
+    }
+    else {
+        cout << "Слово \"" << inputWord << "\" не найдено." << endl;
+    }
+
+    return 0;
+}
